4e5a2d5d924d66b070f688ae7645265fab80705e
IjdiyaouJaafar <ijdiyaoujaafar@gmail.com>
2020-07-21 18:05:20 +0100

    delete command with all attached entities + test and repair some CRUD functions.

--- src/main/java/ma/zs/generated/dao/CommentDao.java
+++ src/main/java/ma/zs/generated/dao/CommentDao.java
@@ -1,19 +1,31 @@
 package ma.zs.generated.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
 import java.util.List;
 
 import ma.zs.generated.bean.Comment;
+import org.springframework.transaction.annotation.Transactional;
 
 
 @Repository
-public interface CommentDao extends JpaRepository<Comment,Long> {
+public interface CommentDao extends JpaRepository<Comment, Long> {
 
 
-       List<Comment> findByCommandReferenceOrderByDateCommentAsc(String reference);
-       int deleteByCommandReference(String reference);       
-       List<Comment> findByCommandId(Long id);
-       int deleteByCommandId(Long id);
+    List<Comment> findByCommandReferenceOrderByDateCommentAsc(String reference);
+
+    int deleteByCommandReference(String reference);
+
+    List<Comment> findByCommandId(Long id);
+
+    @Transactional
+    @Modifying
+    @Query("delete from Comment cm where cm.command.id=:id")
+    int deleteByCommandId(@Param("id") Long id);
+
 
 }
